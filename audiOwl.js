a70f9448f7e77ca6da4ae0e317d7b56d448cf10e
BluSyn <dbcmaster@gmail.com>
2010-12-27 21:22:36 -0800

    Major Updates: Config file now updates through interface, installed templating system + bug fixes

--- audiOwl.js
+++ audiOwl.js
@@ -14,7 +14,8 @@ var sys = require('sys'),
 	fs = require('fs'),
 	db = require('./lib/db.js'),
 	http = require('http'),
-	url = require('url');
+	url = require('url'),
+	jqtpl = require('./lib/jqtpl.js');
 
 /* Load Config Data */
 var cfg = db.read('config');
@@ -25,12 +26,36 @@ http.createServer(function(req, res) {
 	var file = page.pathname == '/' ? 'index.html' : page.pathname;
 	var fpath = basedir+'/htdocs/'+file;
 
+	// Hardcode POST page
+	/// NOTE: replace with better system later ///
+	if (file == '/post') {
+		var resp = { 'error' : 'Invalid Request' }; // Response object
+		var post = page.query; // Query Data
+
+		if (post.action == 'cfg.save') {
+			db.save('config',{
+				ip: post.ip,
+				port: post.port
+			});
+
+			// Reload DB
+			cfg = db.read('config');
+
+			resp = {'message': 'saved'};
+		}
+
+		// Send response as JSON
+		res.writeHead(200, {'Content-Type': 'text/json' });
+		res.end(JSON.stringify(resp));
+	}
+	else {
 	// Attempt to load requested file
-	fs.readFile(fpath, function(err, data) {
+	fs.readFile(fpath, function(err, content) {
 		// Throw 404 on error
 		if (err) {
 			res.writeHead(404, {'Content-Type': 'text/plain'});
 			res.end('Error 404! Page could not be found.');
+			sys.log('[E404] Page not found ('+file+')');
 			return;
 		}
 		
@@ -43,12 +68,20 @@ http.createServer(function(req, res) {
 		else if (file.indexOf('.png') != -1) type = 'image/png';
 		else if (file.indexOf('.jpg') != -1) type = 'image/jpg';
 
+		// If HTML, process with templates
+		if (type == 'text/html') {
+			content = jqtpl.tmpl(content.toString(), {
+				cfg: cfg,
+			});
+		}
+
 		// Send data to browser
 		res.writeHead(200, {'Content-Type': type });
-		res.end(fs.readFileSync(fpath));
+		res.end(content);
 
-		sys.log('[GET] Sent file requested ('+file+'; '+type+')');
+		sys.log('['+req.method+'] Sent file requested ('+file+'; '+type+')');
 	});
+	}
 }).listen(cfg.port, cfg.ip);
 
 sys.log('Server running at '+cfg.ip+':'+cfg.port);
--- htdocs/js/global.js
+++ htdocs/js/global.js
@@ -5,5 +5,23 @@
  */
 
 $(function() {
-	
+	$.reject({
+		reject: { msie: true },
+		close: false,
+		closeESC: false
+	});
+
+	// Handle form submissions
+	$('form').submit(function(e) {
+		$.get('/post', $(this).serialize(), function(data) {
+			console.log(data);
+
+			// Check for error
+			if (data.error) {
+				alert('Error: '+data.error);
+			}
+		}, 'json');
+
+		return false;
+	});
 });
