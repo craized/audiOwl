2b48ea1de87061fc95eff9b6400a99c605691080
BluSyn <dbcmaster@gmail.com>
2011-01-08 18:04:25 -0800

    Improve interface + TV Friendly color scheme. Set up interface for adding library dir. +Library Cleanup

--- audiOwl.js
+++ audiOwl.js
@@ -15,7 +15,7 @@ var sys = require('sys'),
 	db = require('./lib/db.js'),
 	http = require('http'),
 	url = require('url'),
-	jqtpl = require('./lib/jqtpl.js');
+	jqtpl = require('./vendor/jqtpl.js');
 
 /* Load Config Data */
 var cfg = db.read('config');
@@ -33,10 +33,17 @@ http.createServer(function(req, res) {
 		var post = page.query; // Query Data
 
 		if (post.action == 'cfg.save') {
-			db.save('config',{
-				ip: post.ip,
-				port: post.port
-			});
+			// Format fields for DB
+			delete post.action;
+
+			// Handle directory input
+			post.dir = post.dir == undefined ? '' : post.dir;
+			if (typeof post.dir != 'object') {
+				post.dir = post.dir.length > 0 ? [post.dir] : [];
+			}
+
+			// Save DB
+			db.save('config',post);
 
 			// Reload DB
 			cfg = db.read('config');
@@ -49,38 +56,38 @@ http.createServer(function(req, res) {
 		res.end(JSON.stringify(resp));
 	}
 	else {
-	// Attempt to load requested file
-	fs.readFile(fpath, function(err, content) {
-		// Throw 404 on error
-		if (err) {
-			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.end('Error 404! Page could not be found.');
-			sys.log('[E404] Page not found ('+file+')');
-			return;
-		}
-		
-		// Determine MIME TYPE
-		/// NOTE: replace with better system later ///
-		var type = 'text/plain';
-		if (file.indexOf('.html') != -1) type = 'text/html';
-		else if (file.indexOf('.js') != -1) type = 'text/javascript';
-		else if (file.indexOf('.css') != -1) type = 'text/css';
-		else if (file.indexOf('.png') != -1) type = 'image/png';
-		else if (file.indexOf('.jpg') != -1) type = 'image/jpg';
+		// Attempt to load requested file
+		fs.readFile(fpath, function(err, content) {
+			// Throw 404 on error
+			if (err) {
+				res.writeHead(404, {'Content-Type': 'text/plain'});
+				res.end('Error 404! Page could not be found.');
+				sys.log('[E404] Page not found ('+file+')');
+				return;
+			}
+			
+			// Determine MIME TYPE
+			/// NOTE: replace with better system later ///
+			var type = 'text/plain';
+			if (file.indexOf('.html') != -1) type = 'text/html';
+			else if (file.indexOf('.js') != -1) type = 'text/javascript';
+			else if (file.indexOf('.css') != -1) type = 'text/css';
+			else if (file.indexOf('.png') != -1) type = 'image/png';
+			else if (file.indexOf('.jpg') != -1) type = 'image/jpg';
 
-		// If HTML, process with templates
-		if (type == 'text/html') {
-			content = jqtpl.tmpl(content.toString(), {
-				cfg: cfg,
-			});
-		}
+			// If HTML, process with templates
+			if (type == 'text/html') {
+				content = jqtpl.tmpl(content.toString(), {
+					cfg: cfg,
+				});
+			}
 
-		// Send data to browser
-		res.writeHead(200, {'Content-Type': type });
-		res.end(content);
+			// Send data to browser
+			res.writeHead(200, {'Content-Type': type });
+			res.end(content);
 
-		sys.log('['+req.method+'] Sent file requested ('+file+'; '+type+')');
-	});
+			sys.log('['+req.method+'] Sent file requested ('+file+'; '+type+')');
+		});
 	}
 }).listen(cfg.port, cfg.ip);
 
--- htdocs/js/global.js
+++ htdocs/js/global.js
@@ -20,18 +20,72 @@ $(function() {
 			if (data.error) {
 				alert('Error: '+data.error);
 			}
+
+			// Update HTML if sent
+			if (data.html) {
+				for (var x in data.html) {
+					$('#'+x).html(data.html[x]);
+				}
+			}
 		}, 'json');
 
-		return false;
-	})
+		e.preventDefault();
+	});
+
+	// Confirm button
+	$('button.confirm').click(function(e) {
+		var self = $(this);
+		var c = confirm('Are you sure you want to '+self.attr('name').replace(/_/,' ')+'? Can not be un-done.');
+
+		// If confirmation sucessful
+		if (c) {
+			var form = self.parents('form');
+			self.parents('li').remove();
+
+			// Save Changes
+			form.trigger('submit');
+		}
+
+		e.preventDefault();
+	});
+	
+	// SETTINGS PAGE
+	// Handle adding directory to list
+	$('#l_dir').keydown(function(e) {
+		var key = e.which;
+
+		// Enter : 13
+		if (key == '13') {
+			$('#l_add').click();
+			e.preventDefault();
+		}
+	});
+
+	$('#l_add').click(function(e) {
+		var list = $('#dirlist');
+		var dir = $('#l_dir');
+		var val = dir.val();
+
+		// Skip if empty value
+		if (val == '') {
+			e.preventDefault();
+			return;
+		}
+		
+		// Add entry to list
+		var html = '<li>';
+		html += '<input type="hidden" name="dir" value="'+val+'" />';
+		html += '<button name="del_dir" class="confirm key">'+val+'</button></li>';
+		list.append(html);
+
+		// Empty text field
+		dir.val('');
+		
+		// Submit form
+		list.parents('form').trigger('submit');
+		e.preventDefault();
+	});
 	
 	// Start keynav plugin
 	$('.key').keynav('key_focus','key');
-
-	// Handle keycodes
-	// TAB : 9
-	// Arrows: left: 37, up: 38
-	// right: 39, down: 40
-	$('body').keydown(function(e) {
-	});
 });
